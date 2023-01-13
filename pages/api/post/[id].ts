3b7f49695a03f441009ab27603068075f7e8d92f
Bo Ajayi-Walmsley <bo.ajayi-walmsley@mkodo.com>
2023-01-13 15:39:41 +0000

    updated base URL

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -8,11 +8,10 @@ import { postDetailQuery } from '../../../utils/queries';
 export default async function handler( req: NextApiRequest,  res: NextApiResponse) 
 {
     if (req.method === 'GET') {
+        
         const { id } = req.query;
         const query = postDetailQuery(id);
-
         const data = await client.fetch(query);
-        
         res.status(200).json(data[0])
         
     } else if (req.method === 'PUT') {
